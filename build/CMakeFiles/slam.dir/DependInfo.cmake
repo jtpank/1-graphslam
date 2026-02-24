1d85dcd9052b61151d3d25594b53de9a9cc240df
Joshua Lee <joshhlee@stanford.edu>
2026-02-24 06:29:49 -0800

    merging in pose estimation with current...

---
+++ build/CMakeFiles/slam.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joshhlee/Desktop/1-graphslam/src/Frames.cpp" "CMakeFiles/slam.dir/src/Frames.cpp.o" "gcc" "CMakeFiles/slam.dir/src/Frames.cpp.o.d"
+  "/home/joshhlee/Desktop/1-graphslam/src/PoseEstimator.cpp" "CMakeFiles/slam.dir/src/PoseEstimator.cpp.o" "gcc" "CMakeFiles/slam.dir/src/PoseEstimator.cpp.o.d"
+  "/home/joshhlee/Desktop/1-graphslam/src/slam_main.cpp" "CMakeFiles/slam.dir/src/slam_main.cpp.o" "gcc" "CMakeFiles/slam.dir/src/slam_main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/slam.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slam.dir/src/Frames.cpp.o"
+  "CMakeFiles/slam.dir/src/Frames.cpp.o.d"
+  "CMakeFiles/slam.dir/src/PoseEstimator.cpp.o"
+  "CMakeFiles/slam.dir/src/PoseEstimator.cpp.o.d"
+  "CMakeFiles/slam.dir/src/slam_main.cpp.o"
+  "CMakeFiles/slam.dir/src/slam_main.cpp.o.d"
+  "slam"
+  "slam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
